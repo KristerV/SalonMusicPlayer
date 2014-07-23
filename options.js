8c6412cc64094f5c183c602ef2e46eaad39a7766
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-23 13:05:30 +0300

    style and comment changes

--- options.js
+++ options.js
@@ -14,9 +14,6 @@ function getOptions() {
     // correct: 0.4
     // false: 0,4
 
-    // Volume does not work on many tablets. Apple devices have banned
-    // the function, Android devices don't work because of some bug.
-
     //////////////
     // OPTIONS: //
     //////////////
@@ -28,14 +25,14 @@ function getOptions() {
     window.path_to_music = "../../music";
 
     // Nuppude, tekstide suurus
-    // Options: null, 'doublesize', 'quadruplesize'
+    // võimalikud väärutsed: null, 'doublesize', 'quadruplesize'
     window.layout_size = null;
 
     // http://hexcolorgenerator.com/
     window.app_color = '#1a1a2c';
 
     // How long does one session last in minutes
-    window.session_duration_minutes = [20, 40];
+    window.session_duration_minutes = [5, 20, 40];
 
     // Before session ends, for how long does the music gradually turn down volume
     window.session_gradual_end_seconds = 20;
@@ -44,12 +41,12 @@ function getOptions() {
     window.start_popop_delay = 3;
 
     // How long till outro will play (and app will reset) after volume has gone down
-    window.session_outro_delay_minutes = 1;
+    window.session_outro_delay_minutes = 0,05;
 
     // After outro has played, how long until app resets?
-    window.session_reset_delay_minutes = 1;
+    window.session_reset_delay_minutes = 0,3;
 
-    // Is volume slider visible? (true/false)
+    // Is volume slider visible?
     window.volume_slider = false;
 
     // What is the default volume
